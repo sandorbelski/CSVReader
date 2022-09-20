dd4b6de4a7238cbeefed58864597f141e3887752
Aleks Belski <sandorbelski@gmail.com>
2022-09-20 19:25:21 +0000

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World")
+Console.WriteLine("Hello, ameba. CSVReader version 0.0.1");
+
+string filename = "/home/user/Projects/CSVReader/employees.csv";
+
+using (StreamReader reader = new StreamReader(filename))
+{
+    String? line;
+    while ((line = reader.ReadLine()) !=null) {
+            System.Console.WriteLine(line);
+    }
+}
